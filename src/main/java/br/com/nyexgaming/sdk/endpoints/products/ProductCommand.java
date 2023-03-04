c076065c9078b66945543fc2f843e33ed0255146
Wesley G. A. Ramos <dev.wesleygabriel@gmail.com>
2023-03-03 21:09:15 -0300

    .

--- src/main/java/br/com/nyexgaming/sdk/endpoints/products/ProductCommand.java
+++ src/main/java/br/com/nyexgaming/sdk/endpoints/products/ProductCommand.java
@@ -2,7 +2,19 @@ package br.com.nyexgaming.sdk.endpoints.products;
 
 public class ProductCommand {
 
-    public long idx;
-    public String cmd;
+    public long id_comando;
+    public long id_loja;
+    public long id_produto;
+
+    public String comando;
+    public String args;
     public boolean console;
+
+    public boolean temporario;
+    public int dias_duracao;
+
+    public String type;
+
+    public long criado_em;
+    public long atualizado_em;
 }
